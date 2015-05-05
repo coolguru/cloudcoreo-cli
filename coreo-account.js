e2f5b315144c1250de93a38978d2f363c446de3e
Paul Allen <pauldallen@gmail.com>
2015-05-05 01:35:11 -0700

    adding signup process

--- coreo-account.js
+++ coreo-account.js
@@ -4,18 +4,17 @@ var program = require('commander');
 var git_obj = require('./lib/git');
 var helper = require('./lib/helpers');
 var fs = require('fs');
-var prompt = require('prompt');
-var properties = [
-    {
-	name: 'password',
-	hidden: true,
-	required: true,
-	message: 'password should contain at least one number and be between 6 and 16 characters',
-	pattern: /^(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{6,16}$/
-    }
-];
+var read = require('read');
+var request = require('request');
+var mkdirp = require('mkdirp');
+var path = require('path');
+var jsonfile = require('jsonfile');
 
-//prompt.start();
+var signupUrl = 'http://localhost:3000/api/signup';
+
+function getUserHome() {
+  return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+}
 
 program
     .version('0.0.1')
@@ -34,12 +33,52 @@ program
 	    console.error( "The specified directory does not exist");
 	    process.exit(1);
 	}
-	prompt.start();
-	prompt.get(properties, function (err, result) {
-	    if (err) { 
-		return false;
+
+	read({ prompt : 'Password: ', silent : true }, function (err, password) {
+	    if (err) {
+		console.log(err);
 	    }
 	    // do work here
+	    request.post( { 
+		url: signupUrl, 
+		form: {
+		    email: options.email, 
+		    username: options.username, 
+		    betalist: 'true', 
+		    password: password 
+		}
+	    }, function(err, response, body){
+		if(err){
+		    console.log(err);
+		} else {
+		    var bo = JSON.parse(body);
+		    mkdirp.sync(path.join(getUserHome(), '.cloudcoreo'));
+		    var file = path.join(getUserHome(), '.cloudcoreo', 'config');
+		    var config = jsonfile.readFileSync(file);
+		    var configArray = []
+		    // handle user modified screwups array vs. not array etc.
+		    if ( Array.isArray(config) ) {
+			console.log('in an array');
+			for ( var i in config ) {
+			    var c = config[i];
+			    console.log('checking username: ' + c["username"] + ' == ' + bo["username"]);
+			    if (c["username"] == bo["username"]) {
+				// just skip this one because we are going to add it anyway
+				continue;
+			    }
+			    configArray.push(c);
+			}
+		    } else if (config) { // or its there but NOT an array
+			// dont add it if it is the same username
+			if ( ! config["username"] == bo["username"] ) {
+			    configArray.push(config);
+			}
+		    }
+		    // add our new entry
+		    configArray.push(bo);
+		    jsonfile.writeFileSync(file, configArray);
+		}
+	    });
 	});
     })
     .on('--help', function() {
