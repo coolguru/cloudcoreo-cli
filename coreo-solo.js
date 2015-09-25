cb97b765b8735b6d43614f00b584ac82e81b261b
Paul Allen <pauldallen@gmail.com>
2015-09-25 22:03:57 +0200

    fixed up all the things

--- coreo-solo.js
+++ coreo-solo.js
@@ -3,6 +3,7 @@
 var git_obj = require('./lib/git');
 var helper = require('./lib/helpers');
 var accounts = require('./lib/accounts');
+var constants = require('./lib/constants')
 
 var NodeRSA = require('node-rsa');
 var program = require('commander');
@@ -23,78 +24,81 @@ program
     .option("-e, --secret-access-key <secret-access-key>", "The secret access key associated with the corresponding access key id")
     .option("-r, --region <region>", "The region in which this should be launched. If nothing is specified, it will look to launch in the default region supplied by a aws cli config file. If there is no cli config specified, an error will occur.")
     .action(function(options){
-	var mydir = process.cwd();
-	if(options.parent.directory){
-	    mydir = options.parent.directory;
-	}
-	helper.fixConfigYaml(mydir, function(err, data){
-	    if (err) {
-		console.log(err);
-		process.exit(1);
-	    }
-	    
-	    helper.validateRequiredVariables(path.join(mydir, "config.yaml"), function(err, fixed){
-		if(err){
-		    console.log(err);
-		    process.exit(1);
+        var mydir = process.cwd();
+        if(options.parent.directory){
+            mydir = options.parent.directory;
+        }
+        helper.fixConfigYaml(mydir, function(err, data){
+            if (err) {
+                console.log(err);
+                process.exit(1);
+            }
+            
+            helper.validateRequiredVariables(path.join(mydir, "config.yaml"), function(err, fixed){
+                if(err){
+                    console.log(err);
+                    process.exit(1);
+                }
+                var activeConfig = accounts.registerAccountSync(options.profile);
+                if ( ! activeConfig.cloudAccountIdentifier ) { 
+                    activeConfig = accounts.addCloudAccount(activeConfig, options.accessKeyId, options.secretAccessKey, options.region);
 		}
-		var activeConfig = accounts.registerAccountSync(options.profile);
-		activeConfig = accounts.addCloudAccount(activeConfig, options.accessKeyId, options.secretAccessKey, options.region);
-		var repoUrl = activeConfig.username + "@" + activeConfig.sologitaddress + ":/git/" + activeConfig.username + '/solo.git';
-		exec("git remote add ccsolo " + repoUrl, function(err, stdout) {
-		    if (err && err.message.indexOf('already exists') > -1) { 
-			remoteUrl = execSync('git config --get remote.ccsolo.url');
-			if ( remoteUrl != repoUrl) {
-			    console.log('repo has a different repourl - resetting');
-			    console.log('git remote set-url ccsolo ' + repoUrl);
-			    execSync('git remote set-url ccsolo ' + repoUrl);
-			    
-			}
-		    } else if (err) {
-			console.log('ERROR: ' + err);
-			process.exit(1);
-		    }
-		    helper.check_git_config(process.cwd(), function(err, data){ 
-			helper.git_cmd(process.cwd(), activeConfig.privateKeyMaterial, "git add " + process.cwd() + " --all", {}, [], function(err, addOut){
-			    helper.git_cmd(process.cwd(), activeConfig.privateKeyMaterial, "git commit -m \'solo_run\'", {}, [], function(err, commitOut) {
-				helper.git_cmd(process.cwd(), activeConfig.privateKeyMaterial, "git push ccsolo master", {}, [], function(err, pushOut) {
-				    if ( pushOut.trim().indexOf('master -> master') != -1 ) {
-					console.log('changes found and being applied');
-				    } else if (commitOut.trim().indexOf('nothing to commit') > -1 ) {
-					console.log('no changes found - ensuring deployment matches your working directory');
-				    }
-				    var bodyUnEnc = {}
-				    bodyUnEnc.action = "runsolo";
-				    bodyUnEnc.repoUrl = repoUrl;
-				    bodyUnEnc.username = activeConfig.username;
-				    bodyUnEnc.sologitaddress = activeConfig.sologitaddress;
-				    bodyUnEnc.cloudAccountIdentifier = activeConfig.cloudAccountIdentifier;
-				    
-				    var key = new NodeRSA();
-				    key.importKey(activeConfig.privateKeyMaterial, 'private');
-				    
-				    var postForm = {};
-				    postForm.encPayload = key.encryptPrivate(JSON.stringify(bodyUnEnc), 'base64');
-				    postForm.accessKeyId = activeConfig.accessKeyId;
-				    
-				    var headers = {
-					'Content-Type': 'application/json'
-				    };
-				    var urlPath = constants.protocol + '://' + constants.host + ':' + constants.port + '/api/solo';
-				    var res = accounts.mkReq(urlPath, { method: 'POST', headers: headers, body: JSON.stringify(postForm) });
-				    if (res.statusCode == 404){
-					console.log();
-					console.error('something went wrong - it is likely your profile is incorrect or no longer valid');
-					process.exit(1);
-				    }
-				    console.log('any deployment modifications should show up in your cloud account in about 30 seconds');
-				});
-			    });
-			});
-		    });
-		});
-	    });		
-	});
+		
+                var repoUrl = activeConfig.username + "@" + activeConfig.sologitaddress + ":/git/" + activeConfig.username + '/solo.git';
+                exec("git remote add ccsolo " + repoUrl, function(err, stdout) {
+                    if (err && err.message.indexOf('already exists') > -1) { 
+                        remoteUrl = execSync('git config --get remote.ccsolo.url');
+                        if ( remoteUrl != repoUrl) {
+                            console.log('repo has a different repourl - resetting');
+                            console.log('git remote set-url ccsolo ' + repoUrl);
+                            execSync('git remote set-url ccsolo ' + repoUrl);
+                            
+                        }
+                    } else if (err) {
+                        console.log('ERROR: ' + err);
+                        process.exit(1);
+                    }
+                    helper.check_git_config(process.cwd(), function(err, data){ 
+                        helper.git_cmd(process.cwd(), activeConfig.privateKeyMaterial, "git add " + process.cwd() + " --all", {}, [], function(err, addOut){
+                            helper.git_cmd(process.cwd(), activeConfig.privateKeyMaterial, "git commit -m \'solo_run\'", {}, [], function(err, commitOut) {
+                                helper.git_cmd(process.cwd(), activeConfig.privateKeyMaterial, "git push ccsolo master", {}, [], function(err, pushOut) {
+                                    if ( pushOut.trim().indexOf('master -> master') != -1 ) {
+                                        console.log('changes found and being applied');
+                                    } else if (commitOut.trim().indexOf('nothing to commit') > -1 ) {
+                                        console.log('no changes found - ensuring deployment matches your working directory');
+                                    }
+                                    var bodyUnEnc = {}
+                                    bodyUnEnc.action = "runsolo";
+                                    bodyUnEnc.repoUrl = repoUrl;
+                                    bodyUnEnc.username = activeConfig.username;
+                                    bodyUnEnc.sologitaddress = activeConfig.sologitaddress;
+                                    bodyUnEnc.cloudAccountIdentifier = activeConfig.cloudAccountIdentifier;
+                                    
+                                    var key = new NodeRSA();
+                                    key.importKey(activeConfig.privateKeyMaterial, 'private');
+                                    
+                                    var postForm = {};
+                                    postForm.encPayload = key.encryptPrivate(JSON.stringify(bodyUnEnc), 'base64');
+                                    postForm.accessKeyId = activeConfig.accessKeyId;
+                                    
+                                    var headers = {
+                                        'Content-Type': 'application/json'
+                                    };
+                                    var urlPath = constants.protocol + '://' + constants.host + ':' + constants.port + '/api/solo';
+                                    var res = helper.mkReq(urlPath, { method: 'POST', headers: headers, body: JSON.stringify(postForm) });
+                                    if (res.statusCode == 404){
+                                        console.log();
+                                        console.error('something went wrong - it is likely your profile is incorrect or no longer valid');
+                                        process.exit(1);
+                                    }
+                                    console.log('any deployment modifications should show up in your cloud account in about 30 seconds');
+                                });
+                            });
+                        });
+                    });
+                });
+            });         
+        });
     })
     .on('--help', function() {
         console.log('  Examples:');
