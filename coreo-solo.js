16973d33419526e02c53411b7d6facc4feb7c9b7
Paul Allen <pauldallen@gmail.com>
2015-09-25 12:08:00 +0200

    merging coreo-solo.js

--- coreo-solo.js
+++ coreo-solo.js
@@ -1,241 +1,18 @@
 #!/usr/bin/env node
 
-var program = require('commander');
 var git_obj = require('./lib/git');
 var helper = require('./lib/helpers');
-var fs = require('fs');
-var request = require('request');
-var mkdirp = require('mkdirp');
-var path = require('path');
-var jsonfile = require('jsonfile');
-var lineReader = require('line-reader');
-var util = require('util');
-var Table = require('cli-table');
-var readlineSync = require('readline-sync');
-var bcrypt = require('bcrypt-nodejs');
+var accounts = require('./lib/accounts');
+
 var NodeRSA = require('node-rsa');
-var httpSync = require('sync-request');
-var temp = require('temp').track();
+var program = require('commander');
+var request = require('request');
+var Table = require('cli-table');
 var execSync = require('sync-exec');
 var exec = require('child_process').exec
 
-var host = 'www.cloudcoreo.com';
-var protocol = 'https';
-var port = 443;
-var mypath = '/api/solo';
-
 var cloudcoreoGitServer = '';
 var tempIdGeneratorUrl = '';
-
-function readConfigFileSync(configFile){
-    var configs = [];
-    var section = {};
-    if (! fs.existsSync(configFile) ) {
-        return configs;
-    }
-    fs.readFileSync(configFile).toString().split('\n').forEach(function (line) { 
-        var profileRe = new RegExp(/\[(.*)\]/);
-        var profileNames = line.match(profileRe);
-        if ( profileNames ) {
-            // we are in either a new config setting or the first one
-            if ( section.name ) {
-                // if the name has been set, we can add the entire section
-                configs.push(section);
-                // save it off and start the new section
-                section = {};
-            }
-            section.name = profileNames[1].split(' ')[profileNames[1].split(' ').length - 1];
-            section.from = configFile.split(path.sep)[configFile.split(path.sep).length - 1];
-        }
-        var accessKeyRegex = new RegExp(/aws_access_key_id\s*=\s*(.*)\s*$/i);
-        var secretKeyRegex = new RegExp(/aws_secret_access_key\s*=\s*(.*)\s*$/i);
-        var regionRegex = new RegExp(/region\s*=\s*(.*)\s*$/i);
-        accessKeyMatch = line.match(accessKeyRegex);
-        secretKeyMatch = line.match(secretKeyRegex);
-        regionMatch = line.match(regionRegex);
-        
-        if(accessKeyMatch){
-            section.accessKeyId = accessKeyMatch[1];
-        }
-        if(secretKeyMatch){
-            section.secretAccessKey = secretKeyMatch[1];
-        }
-        if(regionMatch){
-            section.region = regionMatch[1];
-        }
-    });
-    if(section.name){
-        configs.push(section);
-    }
-    return configs;
-}
-
-function findAWSCredentials() {
-    var aws_config_file = process.env['AWS_CONFIG_FILE'];
-    var aws_secret_key = process.env['AWS_SECRET_ACCESS_KEY'];
-    var aws_access_key = process.env['AWS_ACCESS_KEY_ID'];
-    var aws_config_dir = getAwsConfigDir();
-
-    configurations = [];
-    if ( aws_secret_key && aws_access_key ) {
-        var memSection = {};
-        memSection.name = 'current';
-        memSection.from = 'environment';
-        memSection.secretAccessKey = aws_secret_key;
-        memSection.accessKeyId = aws_access_key;
-        configurations.push(memSection);
-    }
-
-    if ( fs.existsSync(aws_config_dir)){
-        var configfiles = fs.readdirSync(aws_config_dir);
-        if ( aws_config_file ) {
-            configfiles.push(aws_config_file);
-        }
-        for (var f = 0; f < configfiles.length; f++) {
-            var configs = readConfigFileSync(path.join(aws_config_dir, configfiles[f]));
-            for(var i = 0; i < configs.length; i++) {
-                // lets dedup this stuff
-                var isDup = false;
-                for(var j=0; j < configurations.length; j++) {
-                    if ( configurations[j].name == configs[i].name && configurations[j].accessKeyId == configs[i].accessKeyId ) {
-                        isDup = true;
-                    }
-                }
-                if(! isDup) {
-                    configurations.push(configs[i]);
-                }
-            }
-        }
-    }
-    return configurations;
-}
-function getAwsConfigDir() {
-    return path.join(helper.getUserHome(), '.aws');
-}
-
-function getKeysFromUser() {
-    var keypair = {};
-    var credConfigurations = findAWSCredentials();
-    var useCreds;
-    if ( credConfigurations.length > 0 ) {
-        // there are "on-premise" cloud credentials we can use
-        console.log();
-        console.log('we found credentials on this machine');
-        console.log();
-        console.log('CloudCoreo will use these to create a new role in your account with the');
-        console.log('following policy, which will be assumed to manage resources in your account:');
-        console.log('');
-        console.log('IMPORTANT: we will NEVER store any of your keys in our system');
-        console.log('');
-        console.log('{');
-        console.log('  "Version": "2012-10-17",');
-        console.log('  "Statement": [');
-        console.log('    {');
-        console.log('      "Effect": "Allow",');
-        console.log('      "Action": [');
-        console.log('        "iam:GetUser",');
-        console.log('        "iam:CreatePolicy",');
-        console.log('        "iam:GetPolicy",');
-        console.log('        "iam:CreateRole",');
-        console.log('        "iam:GetRole",');
-        console.log('        "iam:AttachRolePolicy"');
-        console.log('      ],');
-        console.log('      "Resource": "*"');
-        console.log('    }');
-        console.log('  ]');
-        console.log('}');
-        console.log('');
-        console.log('please enter the number corresponding to the account with which you would like to link');
-        console.log('');
-        //lets autogen the table dimensions
-        var numMax = 5;
-        var nameMax = 10;
-        var fromMax = 6;
-        var typeMax = 6;
-        var idMax = 2;
-        var regionMax = 8;
-        var tmpTable = []
-        for ( var i = 0; i < credConfigurations.length; i++ ){
-            var conf = credConfigurations[i];
-            var tblEntry = []
-            tblEntry.push(i);
-            if (i.length + 2 > numMax) { numMax = i.length };
-            tblEntry.push(conf.name);
-            if (conf.name.length + 2 > nameMax) { nameMax = conf.name.length + 2};
-            tblEntry.push(conf.from);
-            if (conf.from && conf.from.length + 2 > fromMax) { fromMax = conf.from.length + 2};
-            if ( ! conf.region ) { 
-                conf.region = 'us-east-1';
-            }
-            tblEntry.push(conf.region);
-            if (conf.region.length + 2 > regionMax) { regionMax = conf.region.length + 2};
-            
-            var fromType = "file";
-            if ( conf.from == "environment" ){
-                fromType = conf.from;
-            }
-            tblEntry.push(fromType);
-            if (fromType.length + 2 > typeMax) { typeMax = fromType.length + 2};
-            
-            tblEntry.push(conf.accessKeyId);
-            if (conf.accessKeyId && conf.accessKeyId.length + 2 > idMax) { idMax = conf.accessKeyId.length + 2};
-	    if ( conf.accessKeyId ) {
-		tmpTable.push(tblEntry);
-	    }
-        }
-        // add everything to a table now
-        var table = new Table({
-            chars: { 'top': '' , 'top-mid': '' , 'top-left': '' , 'top-right': ''
-                     , 'bottom': '' , 'bottom-mid': '' , 'bottom-left': '' , 'bottom-right': ''
-                     , 'left': '' , 'left-mid': '' , 'mid': '' , 'mid-mid': ''
-                     , 'right': '' , 'right-mid': '' , 'middle': ' ' },
-            style: { 'padding-left': 0, 'padding-right': 0 },
-            head: ['Num', 'Name', 'From', 'Region', 'Type', 'ID'],
-            colWidths: [numMax, nameMax, fromMax, regionMax, typeMax, idMax]
-        });
-	tmpTable.push([tmpTable.length, 'Create New', '', '', '', ''])
-        for(var i = 0; i < tmpTable.length; i++){
-            table.push(tmpTable[i]);
-        }
-	
-        console.log(table.toString());
-        console.log('');
-        console.log('press <Enter> to select [0: ' + tmpTable[0][1] + ']');
-        console.log('');
-        
-        var accntNum = readlineSync.question('enter your selection :');
-	if ( ! accntNum || accntNum == "" ){
-	    accntNum = 0;
-	}
-        useCreds = credConfigurations[accntNum];
-    }
-    if(useCreds){
-        keypair.accessKeyId = useCreds.accessKeyId;
-        keypair.secretAccessKey = useCreds.secretAccessKey;
-        keypair.region = useCreds.region;
-        return keypair;
-    } else {
-        console.log('no existing credentials specified - you must supply new ones');
-        console.log('');
-        
-        var accessKeyId = readlineSync.question('enter a access key id :');
-        var secretAccessKey = readlineSync.question('enter your secret access key (we do not store it) :');
-        var region = readlineSync.question('enter a default region :');
-
-        keypair.accessKeyId = accessKeyId;
-        keypair.secretAccessKey = secretAccessKey;
-        keypair.region = region;
-        return keypair;
-        
-    }
-}
-
-function mkReq(path, options) {
-    options = options || {};
-    var url = protocol + '://' + host + ':' + port + '/' + path;
-    var response = httpSync(options.method, url, options);
-    return response;
-}
 
 program
     .command('run')
@@ -260,123 +37,9 @@ program
 		    console.log(err);
 		    process.exit(1);
 		}
-		
-		// check if there is already a config file if not, sign up and create
-		var key = new NodeRSA();
-		
-		var configs = helper.getConfigArray();
-		var profileName = options.profile || 'default';
-		var activeConfig;
-		var postForm = {};
-		var accessKeyId;
-		var secretAccessKey;
-		var cloudAccountIdentifier;
-		var configs = helper.getConfigArray();
-		if(configs.length > 0) {
-		    // lets use a config that we found, get by name
-		    activeConfig = configs[0];
-		} 
-		if ( ! activeConfig || (activeConfig && ! activeConfig.accessKeyId)) { 
-		    // if there is no config, or
-		    // there is an active config but no accessKeyId
-		    // then we need to go up and register
-		    
-		    // we still have no config, so we need to do a key exchange.
-		    var keyPair = key.generateKeyPair(2048);
-		    postForm.publicKeyMaterial = keyPair.exportKey('public')
-		    
-		    var headers = {
-			'Content-Type': 'application/json'
-		    };
-		    var res = mkReq('api/solo', { method: 'POST', headers: headers, body: JSON.stringify(postForm) });
-		    // need to register our cloud account info
-		    if (res.statusCode == 404){
-			console.log('there was a problem with our servers');
-			process.exit(1);
-		    }
-		    
-		    activeConfig = JSON.parse(res.body.toString());
-		    activeConfig.publicKeyMaterial = postForm.publicKeyMaterial;
-		    activeConfig.privateKeyMaterial = keyPair.exportKey('private');
-		    
-		    helper.addConfig(activeConfig);
-		    
-		}
-		// go get the config again
-		var configs = helper.getConfigArray();
-		if(configs.length > 0) {
-		    // lets use a config that we found, get by name eventaully - now just one...
-		    activeConfig = configs[0];
-		} 
-		// we are certianly registered now, lets make sure we have a cloud account registered
-		if ( ! activeConfig.cloudAccountIdentifier ) { 
-		    if ( ! options.accessKeyId || ! options.secretAccessKey || ! options.region) {
-			// if we are here we either need to have an access key and a secret key
-			console.log('you must supply ALL OF: aws access key id, secret access key, region');
-			var keypair = getKeysFromUser();
-			if ( keypair.accessKeyId.length < 1 || keypair.secretAccessKey.length < 1 ) {
-			    console.log('cannot proceed with missing or invalid cloud credentials');
-			    process.exit(1);
-			}
-			// at this point we have a good set of keys to try out
-			cloudAccountIdentifier = bcrypt.hashSync(keypair.accessKeyId);
-			accessKeyId = keypair.accessKeyId;
-			secretAccessKey = keypair.secretAccessKey;
-			region = keypair.region;
-		    } else {
-			// else just get the ones passed in on the command line
-			cloudAccountIdentifier = bcrypt.hashSync(options.accessKeyId);
-			accessKeyId = options.accessKeyId;
-			secretAccessKey = options.secretAccessKey;
-			region = options.region
-		    }
-		    // we have everything we need - lets post up the encrypted payload
-		    var bodyUnEnc = {};
-		    bodyUnEnc.cloudAccountIdentifier = cloudAccountIdentifier;
-		    bodyUnEnc.accessKeyId = accessKeyId;
-		    bodyUnEnc.secretAccessKey = secretAccessKey;
-		    bodyUnEnc.region = region;
-		    
-		    var key = new NodeRSA();
-		    key.importKey(activeConfig.privateKeyMaterial, 'private');
-		    postForm = {};
-		    postForm.encPayload = key.encryptPrivate(JSON.stringify(bodyUnEnc), 'base64');
-		    postForm.accessKeyId = activeConfig.accessKeyId;
-		    
-		    var headers = {
-			'Content-Type': 'application/json'
-		    };
-		    var res = mkReq('api/solo', { method: 'POST', headers: headers, body: JSON.stringify(postForm) });
-		    if (res.statusCode == 404){
-			console.log('your cloud account registration was not found our the system');
-			process.exit(1);
-		    }
-		    
-		    var creds = JSON.parse(res.body.toString());
-		    if ( creds.arn ) {
-			console.log('new role created in the cloud account: ' + creds.arn);
-		    } else { 
-			console.log('something went wrong - are you sure those cloud credentials are correct?');
-			process.exit(1);
-		    }
-		    
-		    newActiveConfig = helper.clone(activeConfig);
-		    newActiveConfig.region = region;
-		    newActiveConfig.cloudAccountIdentifier = cloudAccountIdentifier;
-		    newActiveConfig.arn = creds.arn;
-		    helper.updateConfig(activeConfig, newActiveConfig);
-		}
-		// at this point we have a cloudAccountIdentifier that exists in cloudcoreo
-		// and/or we have access keys and a cloudAccountIdentifier that doesn't exist yet
-		// go get the config again
-		var configs = helper.getConfigArray();
-		if(configs.length > 0) {
-		    // lets use a config that we found, get by name eventaully - now just one...
-		    activeConfig = configs[0];
-		} 
-		
+		var activeConfig = accounts.registerAccountSync(options.profile);
+		activeConfig = accounts.addCloudAccount(activeConfig, options.accessKeyId, options.secretAccessKey, options.region);
 		var repoUrl = activeConfig.username + "@" + activeConfig.sologitaddress + ":/git/" + activeConfig.username + '/solo.git';
-		
 		exec("git remote add ccsolo " + repoUrl, function(err, stdout) {
 		    if (err && err.message.indexOf('already exists') > -1) { 
 			remoteUrl = execSync('git config --get remote.ccsolo.url');
@@ -416,7 +79,8 @@ program
 				    var headers = {
 					'Content-Type': 'application/json'
 				    };
-				    var res = mkReq('api/solo', { method: 'POST', headers: headers, body: JSON.stringify(postForm) });
+				    var urlPath = constants.protocol + '://' + constants.host + ':' + constants.port + '/api/solo';
+				    var res = accounts.mkReq(urlPath, { method: 'POST', headers: headers, body: JSON.stringify(postForm) });
 				    if (res.statusCode == 404){
 					console.log();
 					console.error('something went wrong - it is likely your profile is incorrect or no longer valid');
@@ -428,24 +92,7 @@ program
 			});
 		    });
 		});
-		
-		// no config file that we can use - creating a new account now
-		// // re-get the config array now that we have made it through the new signup process.
-		// config = helper.getConfigArray();
-		// one with temp creds. This is simply so the process doesn't have to continue
-		// over and over again if they want to use solo more often.
-		
-		// now there is a config file, either old or new it doesnt matter
-		// check if there is a cloud account associated with the config file
-		
-		// use the new keys to set up the cloud account and associate it with the values from the config file
-		// now the cloud account is set up and associated w/ this temp user in CC
-		
-		// print out variables that will be used to give the user an opportunity to change them
-		// flag variables that are not defaulted
-		// git push the current dir to the temp git repo server in cloudcoreo
-		// send a post to cloudcoreo.com to kick off the process.
-	    });
+	    });		
 	});
     })
     .on('--help', function() {
